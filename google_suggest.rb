65dac615c630afe391dae7b22d5ab73db82d0db0
janpapenbrock <lunoxx@gmail.com>
2013-08-26 22:29:09 +0200

    Some magic to generate more suggestions.

--- google_suggest.rb
+++ google_suggest.rb
@@ -1,13 +1,33 @@
+# Encoding: UTF-8
+
 require 'open-uri'
 require 'json'
 
 class GoogleSuggest
+
+	MAXIMUM_NUMBER_OF_SUGGESTIONS = 14
 
 	attr_accessor :result
 
 	def self.suggest phrase, language_code, depth = 999
 		i = self.new
 		i.search(phrase, language_code, depth)
+	end
+
+	def self.create_suggest_permutations phrase
+		("a".."z").collect{|letter| "#{phrase} #{letter}"}
+	end
+
+	def self.create_permutation_parts suggestion, phrase
+		parts = suggestion.split " "
+		joined_parts = []
+		permutations = []
+		parts.each do |part|
+			joined_parts << part
+			permutation = joined_parts.join(" ")
+			permutations << permutation if permutation > phrase
+		end
+		permutations
 	end
 
 	def initialize
@@ -21,23 +41,49 @@ class GoogleSuggest
 		uri_phrase = URI.escape(phrase, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
 		url = "http://google.com/complete/search?client=chrome&q=#{uri_phrase}&hl=#{language_code}"
 
-		p url
-
 		suggest_contents = {}
+		sleep 0.15
 		open(url) do |f|
-			suggest_contents = JSON.parse f.read
+			contents = f.read
+			suggest_contents = JSON.parse contents
 		end
-		suggestions = suggest_contents[1]
+		suggestions = suggest_contents[1].flatten
+		
+		need_full_permutations = (suggestions.count == self.class::MAXIMUM_NUMBER_OF_SUGGESTIONS)
 
 		new_suggestions = suggestions - @found
-		@found += new_suggestions
+		if new_suggestions
+			@found += new_suggestions
 
-		new_suggestions.each do |new_suggestion|
-			search new_suggestion, language_code, depth - 1
+			additional_suggestions = generate_additional_suggestions(suggestions, phrase, need_full_permutations)
+		
+			additional_suggestions.each do |new_suggestion|
+				search new_suggestion, language_code, depth - 1
+			end
 		end
 
 		self.result[:results] = @found
 		self.result
+	end
+
+	def generate_additional_suggestions suggestions, phrase, need_full_permutations=false
+		return [] unless suggestions
+		
+		alphabet_permutations = self.class.create_suggest_permutations(phrase)
+		part_permutations 	  = suggestions.collect{|suggestion| self.class.create_permutation_parts(suggestion, phrase)}
+		
+		suggestions += part_permutations
+		suggestions += alphabet_permutations if need_full_permutations
+
+		suggestions.flatten!
+
+		return [] unless suggestions
+		suggestions.uniq!
+		return [] unless suggestions
+
+		suggestions -= @found
+
+		suggestions
 	end
 
 end
